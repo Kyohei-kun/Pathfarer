229b58942f8826f475fc333e441edee9494c865f
Kyohei-kun <cedric.gazzano@gmail.com>
2023-05-12 10:08:06 +0200

    Loader Script Editor

---
+++ Assets/Editor/CS_LoadSceneEditor.cs
@@ -0,0 +1,23 @@
+﻿using NaughtyAttributes;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CS_LoadSceneEditor : MonoBehaviour
+{
+
+    [ReorderableList] public List<SceneAsset> scenes = new List<SceneAsset>();
+
+    [Button("■■■■ Load Scenes ■■■■")]
+    public void OpenMainScene()
+    {
+        EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo();
+        foreach (var item in scenes)
+        {
+            EditorSceneManager.OpenScene(AssetDatabase.GetAssetPath(item), OpenSceneMode.Additive);
+        }
+    }
+}
