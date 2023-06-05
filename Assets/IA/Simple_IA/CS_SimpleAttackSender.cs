607bfb5820b270eb7484f8b2e252200ea9a6c5c7
Armelle <armelle.caillo.pro@gmail.com>
2023-06-05 14:29:05 +0200

    Serialize value script attaque IA

--- Assets/IA/Simple_IA/CS_SimpleAttackSender.cs
+++ Assets/IA/Simple_IA/CS_SimpleAttackSender.cs
@@ -1,3 +1,4 @@
+using NaughtyAttributes;
 using StarterAssets;
 using System.Collections;
 using System.Collections.Generic;
@@ -5,14 +6,17 @@ using UnityEngine;
 
 public class CS_SimpleAttackSender : MonoBehaviour
 {
+    [MinValue(0)][SerializeField] float knockbackForce = 100;
+    [Required][SerializeField] Transform enemy;
+
     private void OnTriggerEnter(Collider other)
     {
         CS_PlayerLife playerLife = other.GetComponent<CS_PlayerLife>();
         if(playerLife != null)
         {
             playerLife.LoseLife();
-            Vector3 pushVector = transform.parent.parent.forward;
-            other.GetComponent<ThirdPersonController>().Push(pushVector, 100f);
+            Vector3 pushVector = enemy.forward;
+            other.GetComponent<ThirdPersonController>().Push(pushVector, knockbackForce);
         }
     }
 }
